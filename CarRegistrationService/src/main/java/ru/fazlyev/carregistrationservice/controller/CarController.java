12f4231d35b301c65230f0fc2960b030a22e6519
Azat Fazlyev <fazliev14@gmail.com>
2021-03-02 16:03:17 +0300

    Finished Car Db service and some fixes in car registration service

--- CarRegistrationService/src/main/java/ru/fazlyev/carregistrationservice/controller/CarController.java
+++ CarRegistrationService/src/main/java/ru/fazlyev/carregistrationservice/controller/CarController.java
@@ -1,21 +1,29 @@
 package ru.fazlyev.carregistrationservice.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.fazlyev.carregistrationservice.dto.CarDto;
 import ru.fazlyev.carregistrationservice.service.CarService;
 
 @Controller
 public class CarController {
-    private CarService carService;
+    private final CarService carService;
 
     public CarController(CarService carService) {
         this.carService = carService;
     }
 
     @PostMapping("/cars")
-    public String saveRegistration(@RequestBody CarDto carDto){
+    public String saveRegistration(@RequestParam String brand, String color){
+        CarDto carDto = CarDto.builder()
+                .brand(brand)
+                .color(color)
+                .build();
+
         carService.registerCar(carDto);
         return "success";
     }
