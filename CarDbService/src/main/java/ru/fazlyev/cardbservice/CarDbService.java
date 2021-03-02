8e0e4c1b97e0423189ac99e56573ac306790c1e7
Azat Fazlyev <fazliev14@gmail.com>
2021-03-02 13:36:46 +0300

    Initial commit

---
+++ CarDbService/src/main/java/ru/fazlyev/cardbservice/CarDbService.java
@@ -0,0 +1,11 @@
+package ru.fazlyev.cardbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDbService {
+    public static void main(String[] args) {
+        SpringApplication.run(CarDbService.class, args);
+    }
+}
---
+++ CarRegistrationService/src/main/java/ru/fazlyev/carregistrationservice/CarRegistrationMain.java
@@ -0,0 +1,11 @@
+package ru.fazlyev.carregistrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRegistrationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(CarRegistrationMain.class, args);
+    }
+}
